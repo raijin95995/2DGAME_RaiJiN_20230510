b6916907e1fa8f7a3b3c00790366c14514a924bb
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2023-05-23 21:21:59 +0800

    0523

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class ControlSystem : MonoBehaviour
 {
+    int A = 5;
+    int B = 6;
+
+    private void Awake()
+    {
+        print(1 + 1);
+        print($"{2^2 + 3}");
+        print($"A+B={A + B}" );
+        print(A + B);
+        print("A+B=" + (A + B));
+        print("A+B=" + A + B);
+    }
+
+    private void Start()
+    {
+        print("Im Start");
+    }
+
+    private void Update()
+    {
+        print("<color=red>Øp</color>");
+    }
+
 
 }
