d986667d8561395a4afbed44152aa0d75578dc0e
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2023-05-25 19:20:03 +0800

    0525

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -4,26 +4,36 @@ public class ControlSystem : MonoBehaviour
 {
     int A = 5;
     int B = 6;
+    [Header("移動速度"),Range(0,10)]
+    public float moveSpeed = 3.0f;
+    [Header("角色剛體")]
+    public Rigidbody2D rig;
+
 
     private void Awake()
     {
-        print(1 + 1);
-        print($"{2^2 + 3}");
-        print($"A+B={A + B}" );
-        print(A + B);
-        print("A+B=" + (A + B));
-        print("A+B=" + A + B);
+        //print(1 + 1);
+        //print($"{2^2 + 3}");
+        //print($"A+B={A + B}" );
+        //print(A + B);
+        //print("A+B=" + (A + B));
+        //print("A+B=" + A + B);
     }
 
     private void Start()
     {
-        print("Im Start");
+        //print("Im Start");
     }
 
     private void Update()
     {
-        print("<color=red>喵</color>");
+        //print("<color=red>喵</color>");
+        Move();
     }
 
+	private void Move()
+	{
+        print("I'm walking");
+	}
 
 }
