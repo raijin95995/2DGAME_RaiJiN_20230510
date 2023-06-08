dfcd4b7551a02f4c99de8aea818de763c50050c1
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2023-06-08 21:16:56 +0800

    被包夾了

---
+++ Assets/Scripts/MonsterSystem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MonsterSystem : MonoBehaviour
+{
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("主人公選取").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, 0.05f);
+    }
+
+}
